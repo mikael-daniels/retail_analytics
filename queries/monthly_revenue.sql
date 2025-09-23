e1826353ec7397066b2ac33867b9f2f56f1f3974
Mikael Daniels <mikael@kuunda.com>
2025-09-23 20:08:57 +0200

    Clean SQL code

--- queries/monthly_revenue.sql
+++ queries/monthly_revenue.sql
@@ -1,3 +1,9 @@
-select FORMAT_DATE('%Y-%m', DATE(sale_date)) as sale_month, sum(amount) as total_amount from analytics.fact_sales
-group by 1
-order by 1;
+select
+  FORMAT_DATE('%Y-%m', DATE(sale_date)) as sale_month,
+  sum(amount) as total_amount
+from
+  analytics.fact_sales
+group by
+  1
+order by
+  1;
--- queries/top_customers.sql
+++ queries/top_customers.sql
@@ -1,5 +1,12 @@
-select c.customer_name, sum(s.amount) as total_amount from analytics.fact_sales s
-left join analytics.dim_customers c on s.customer_id = c.customer_id
-group by c.customer_name
-order by 2 DESC
-LIMIT 10;
+select
+  c.customer_name,
+  sum(s.amount) as total_amount
+from
+  analytics.fact_sales s
+  left join analytics.dim_customers c on s.customer_id = c.customer_id
+group by
+  c.customer_name
+order by
+  2 DESC
+LIMIT
+  10;
